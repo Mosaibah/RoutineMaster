b34d0fa49fcf45242fb9f19995e9fed268094659
Abdulrahman <abudrm2020@gmail.com>
2023-04-14 16:08:02 +0300

    add react-chartjs-2 #5

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,8 @@ import Footer from '../components/Footer';
 import Card from '../components/Card';
 import { useEffect, useState } from 'react';
 import { Auth } from 'aws-amplify';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Pie } from 'react-chartjs-2';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -55,6 +57,34 @@ export default function Home() {
     console.log('Card selected');
   };
 
+  ChartJS.register(ArcElement, Tooltip, Legend);
+  const data = {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [
+      {
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
   return (
   <>
       <div className=' px-2 py-4 bg-gray-300 font-white mt-4 mx-4 rounded-md text-center text-lg font-semibold'>
@@ -67,7 +97,7 @@ export default function Home() {
                   tasks={template.tasks} onSelect={handleSelect} 
                   typeOfButton="Select"
                   color="green"/>
-          )): <span></span>}
+          )): <Pie data={data} />}
         </div>
       </div>
   </>
